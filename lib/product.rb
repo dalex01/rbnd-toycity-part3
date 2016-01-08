ab788092501793c1f1a6d57ef14e1442be851b76
Alexey Drozdov <alexey.drozdov@gmail.com>
2016-01-08 22:50:08 +0300

    Add check on the same product adding

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,30 @@
 class Product
+  attr_reader :title
 
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  def self.find_by_title(title)
+  	@@products.each do |product|
+  		return product if product.title == title
+  	end
+  end
+
+  private
+
+  def add_to_products
+  	@@products.each do |product|
+    	raise DuplicateProductError, "#{@title} already exists." if product.title == @title
+    end
+   	@@products << self
   end
 
 end
