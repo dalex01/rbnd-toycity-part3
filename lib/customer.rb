c487d630dd93ac8215a71cb698804b98827bfa50
Alexey Drozdov <alexey.drozdov@gmail.com>
2016-01-08 23:27:46 +0300

    Implement Customer class

---
+++ lib/customer.rb
@@ -0,0 +1,28 @@
+class Customer
+	attr_reader :name
+
+	@@customers = []
+
+	def initialize(options={})
+		@name = options[:name]
+		add_to_customers
+	end
+
+	def self.all
+    	@@customers
+  	end
+
+	def self.find_by_name(name)
+  		@@customers.each {|customer| return customer if customer.name == name}
+  	end
+
+  	private
+
+  	def add_to_customers
+  		@@customers.each do |customer|
+    		raise DuplicateProductError, "#{@name} already exists." if customer.name == @name
+    	end
+   		@@customers << self
+  	end
+
+end
