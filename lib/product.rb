de10e2458a1569c75f08a8c7657a05c10f1f720d
Alexey Drozdov <alexey.drozdov@gmail.com>
2016-01-08 23:06:44 +0300

    Implement Product class with self.find_by_title, in_stock?, self.in_stock methods and price and stock properties

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,12 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_to_products
   end
 
@@ -13,9 +15,17 @@ class Product
   end
 
   def self.find_by_title(title)
-  	@@products.each do |product|
-  		return product if product.title == title
-  	end
+  	@@products.each {|product| return product if product.title == title}
+  end
+
+  def in_stock?
+  	return @stock > 0 ? true : false
+  end
+
+  def self.in_stock
+  	products_in_stock = []
+  	@@products.each {|product| products_in_stock.push(product) if product.in_stock?}
+  	return products_in_stock
   end
 
   private
